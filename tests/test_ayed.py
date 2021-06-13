d4aedbe116110e4165ad25549090b7db5328fc39
Bocanada <24578415+Bocanada@users.noreply.github.com>
2021-06-13 09:06:14 -0300

    added more tests

--- tests/test_ayed.py
+++ tests/test_ayed.py
@@ -6,7 +6,7 @@ from ayed import Tokenizer
 def test_version() -> None:
     assert __version__ == "0.1.0"
 
-def test_from_str() -> None:
+def test_tfrom_str() -> None:
     result = """Equipo equipoFromString(string e)
 {
 Equipo x;
@@ -22,6 +22,26 @@ return x;
     t = Tokenizer.from_path(Path('tests/structs/structs.cpp'))
     assert str(t.tokens[0].from_str()) == result
 
+def test_from_str() -> None:
+    result = """Equipo equipoFromString(string e)
+{
+Equipo x;
+string t0 = getTokenAt(e, '-', 0);
+x.idEq = stoi(t0);
+string t1 = getTokenAt(e, '-', 1);
+strcpy(x.nombre, t1.c_str());
+string t2 = getTokenAt(e, '-', 2);
+x.puntos = stoi(t2);
+return x;
+};
+"""
+    tr = """struct Equipo {
+  int idEq;
+  char nombre[20];
+  int puntos;
+};"""
+    t = Tokenizer.from_str(tr)
+    assert str(t.tokens[0].from_str()) == result
 def test_to_str() -> None:
     result = """string equipoToString(Equipo e)
 {
