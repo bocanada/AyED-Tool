24f2e70484d7c774b860546a266429ff0c960d96
Bocanada <24578415+Bocanada@users.noreply.github.com>
2021-06-13 15:11:37 -0300

    added an uglier but more robust regexp

--- ayed/main.py
+++ ayed/main.py
@@ -15,7 +15,11 @@ from ayed.classes import Struct, Variable
 from typing import Any
 
 nfields = compile(
-    r'struct\s+(?P<struct>\w+)\s+{|(?P<type>[\w\s]*)\s+(?P<identifier>\w+)(?P<cstr>\[\d*\])?;\s*?|(?P<ENDLINE>};)'
+    r'struct (?P<struct>\w+)\s{|\s*'  # struct name
+    r'(?P<type>[char|signed char|unsigned char|short|short int|signed short|signed short int|unsigned short'  # data types
+    r'|unsigned short int|int|signed|signed int|unsigned|unsigned int|long|long int|signed long|signed long int|unsigned long|unsigned long int'
+    r'|long long|long long int|signed long long|signed long long int|unsigned long long|unsigned long long int|float|double|long double|\w]+)\s'
+    r'(?P<identifier>\w+)(?P<cstr>\[\d*\])?;\s*?|(?P<ENDLINE>};)*'  # variable name | char array | };
 )
 
 
@@ -43,6 +47,8 @@ class Tokenizer:
             if endl:
                 tokens.append(Struct(**struct))
                 struct['fields'] = []
+                continue
+            if not any([sname, ttype, vname, ctype]):
                 continue
             if sname:
                 struct['name'] = sname.strip()
